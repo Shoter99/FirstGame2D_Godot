1c61d2c0c173e2f7ba62a6653a66753c81239373
Vivi <shoter998@gmail.com>
2022-05-23 22:58:31 +0200

    added enemy and player, need fix: player not showing on the scene

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,7 @@ var screen_size
 
 
 func _ready():
+	show()
 	screen_size = get_viewport_rect().size
 
 func _physics_process(delta):
@@ -13,7 +14,29 @@ func _physics_process(delta):
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = velocity.normalized() * speed * delta * 100
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
+	move_and_slide(velocity)
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Hitbox_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
